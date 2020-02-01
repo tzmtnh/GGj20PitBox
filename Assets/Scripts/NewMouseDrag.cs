ad5e04047c3ea00a86ef5274895a5c616cd4a9c7
NukoTheNeko <36847623+NukoTheNeko@users.noreply.github.com>
2020-02-01 18:44:37 +0000

    Wheels

--- Assets/Scripts/NewMouseDrag.cs
+++ Assets/Scripts/NewMouseDrag.cs
@@ -8,12 +8,15 @@ public class NewMouseDrag : MonoBehaviour
 
     public GameObject selected;
     public LayerMask layerMask;
-
+    public GameObject wheal;
     public float whealHeight = 0.5f;
     public float RedThingHeight = 0.5f;
 	public float gasHeight = 0.5f;
 
 	bool _waitForMouseRelease = false;
+
+    [SerializeField] private Transform[] _wheels = new Transform[4];
+    private bool[] _wheelsMoved = {false, false, false, false};
 
 	public void WaitForMouseRelease() {
 		_waitForMouseRelease = true;
@@ -23,11 +26,19 @@ public class NewMouseDrag : MonoBehaviour
 		inst = this;
 	}
 
-	void Update()
+    public void Reset()
+    {
+        for (int i = 0; i >= 3; i++)
+        {
+            _wheelsMoved[i] = false;
+        }
+    }
+
+    void Update()
     {
 		bool isLeftMouseButtonPressed = Input.GetMouseButton(0);
 
-		if (_waitForMouseRelease == false && isLeftMouseButtonPressed)
+        if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -35,17 +46,43 @@ public class NewMouseDrag : MonoBehaviour
             if (Physics.Raycast(ray, out hit, 1000, layerMask))
             {
                 if (selected == null)
-                { 
-                    if(	hit.transform.tag == "Wheal" ||
+                {
+                    if (hit.transform.tag == "WheelMesh")
+                    {
+                        for (int i=0; i<=3; i++)
+                        { 
+                            Debug.Log(_wheelsMoved[i]);
+                            if (_wheels[i] == hit.transform && _wheelsMoved[i]==false)
+                            {
+                                _wheelsMoved[i] = true;
+                                Debug.Log("Found Mesh");
+                                hit.transform.GetComponentInParent<FixWhealInPlace>().itsOk = true;
+                                hit.transform.gameObject.SetActive(false);
+                                Instantiate(wheal, hit.transform.position, wheal.transform.rotation).GetComponent<WhealStatus>()
+                                    .theyAreNew = false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (_waitForMouseRelease == false && isLeftMouseButtonPressed)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 1000, layerMask))
+            {
+                if (selected == null)
+                {
+                    if (	hit.transform.tag == "Wheal" ||
 					    hit.transform.tag == "RedThing" ||
 						hit.transform.tag == "Gas")
                     {
                         selected = hit.transform.gameObject;
                     }
-                    else if (hit.transform.tag=="WheelMesh")
-                    {
-
-                    }
+                    
                 }
 
 				if (selected != null) {
