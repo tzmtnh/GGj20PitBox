cff11f956fe34d00c8f5557c9a391c7c7b62e9f3
Matan <matan@onehamsa.com>
2020-02-01 01:49:24 +0000

    Basic camera rig

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public float sensitivity = 2;
+	public float drag = 0.5f;
+
+	Camera _camera;
+	Transform _pitch;
+	Vector2 _lastPos;
+
+	float _orbitAngle;
+	float _orbitVelocity;
+
+	float _pitchAngle;
+	float _pitchVelocity;
+
+	void Awake() {
+		_pitch = transform.Find("Camera Pitch");
+		_camera = _pitch.GetComponentInChildren<Camera>();
+	}
+
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			_lastPos = Input.mousePosition;
+		}
+
+		float dt = Time.deltaTime;
+		if (Input.GetMouseButton(0)) {
+			Vector2 pos = Input.mousePosition;
+			Vector2 delta = pos - _lastPos;
+
+			_orbitVelocity = delta.x * sensitivity;
+			_pitchVelocity = -delta.y * sensitivity;
+		} else {
+			_orbitVelocity *= (1f - drag);
+			_pitchVelocity *= (1f - drag);
+		}
+
+		_orbitAngle += _orbitVelocity * dt;
+		_pitchAngle += _pitchVelocity * dt;
+		_pitchAngle = Mathf.Clamp(_pitchAngle, 5, 90);
+
+		transform.localRotation = Quaternion.Euler(0, _orbitAngle, 0);
+		_pitch.localRotation = Quaternion.Euler(_pitchAngle, 0, 0);
+	}
+}
