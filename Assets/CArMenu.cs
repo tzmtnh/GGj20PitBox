71486bcf7dfd16c8e5d62a8a87da6dca0087d660
NukoTheNeko <36847623+NukoTheNeko@users.noreply.github.com>
2020-02-02 16:02:07 +0000

    stuff

---
+++ Assets/CArMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CArMenu : MonoBehaviour
+{
+    [SerializeField]
+    private MainMenu _mm;
+    public void StartGame()
+    {
+        _mm.PlayGame();
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,9 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Animator _car;
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+        StartCoroutine(Wait());
+    }
+
+    private IEnumerator Wait()
+    {
+        _car.SetTrigger("go");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
